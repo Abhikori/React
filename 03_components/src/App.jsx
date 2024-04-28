29009f8f312a85b2b2e3c2ff6515d4f4e5766ddc
Abhishek <abhishekkori7902@gmail.com>
2024-04-28 15:15:29 +0530

    components

---
+++ 03_components/src/App.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+import Random from "./Random";
+
+function App() {
+  return <div>
+    <h1>
+      React Learning
+    </h1>
+    <Button/>
+    <br />
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+  </div>
+}
+
+export default App;
